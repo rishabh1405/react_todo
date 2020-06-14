477d5250a53845c72af0a23b7c6d4dcbad0bec5a
rishabh <rishabh.singh@delta.in>
2020-06-14 15:38:27 +0530

    Updates

---
+++ precache-manifest.89cbb3dceca87f501b1bfcdf2d63e422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5265e955bb2192d49349",
+    "url": "/react_todo/static/css/main.bd80e05d.chunk.css"
+  },
+  {
+    "revision": "5265e955bb2192d49349",
+    "url": "/react_todo/static/js/main.0f200817.chunk.js"
+  },
+  {
+    "revision": "fe35fca0fea852899367",
+    "url": "/react_todo/static/js/runtime~main.db2a12cb.js"
+  },
+  {
+    "revision": "6429b6b9a55de5b7e9e6",
+    "url": "/react_todo/static/js/2.b8eee446.chunk.js"
+  },
+  {
+    "revision": "66d6baa034206006400f580642237a74",
+    "url": "/react_todo/index.html"
+  }
+];
